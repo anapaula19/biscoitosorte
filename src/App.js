2729b927ce107cde4c0f9176fa9ab0c6adbdee6b
anapaula19 <95650444+anapaula19@users.noreply.github.com>
2022-03-11 21:01:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './estilo.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: ''
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = [
+      'A vida trará coisas boas se tiveres paciência.',
+      'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+      'A maior de todas as torres começa no solo.',
+      'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.',
+      'Não compense na ira o que lhe falta na razão.',
+      'Não há que ser forte. Há que ser flexível.',
+      'Siga os bons e aprenda com eles.',
+      'Não importa o tamanho da montanha, ela não pode tapar o sol.',
+      'O bom-senso vai mais longe do que muito conhecimento.',
+      'Quem quer colher rosas deve suportar os espinhos.',
+      'São os nossos amigos que nos ensinam as mais valiosas lições.'
+    ];
+  }
+/*
+
+*/
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "';
+    this.setState({state});
+  }
+  
+  render(){
+    return(
+      <div className="container">
+        <img src={require('./assets/biscoito.png')} className="img"/>
+        <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
+}
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button className="botao botao1" onClick={this.props.acaoBtn} >{this.props.nome}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
